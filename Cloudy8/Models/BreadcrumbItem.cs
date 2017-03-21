98566031559b028ce2278c124a1d289deba4db7c
Jawad Sabra <jawad.sabra@avanade.com>
2017-03-21 13:17:47 +0000

    Side menu and Breadcrumb

---
+++ Cloudy8/Models/BreadcrumbItem.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Links;
+
+namespace Cloudy8.Models
+{
+    public class BreadcrumbItem : Sitecore.Data.Items.CustomItem
+    {
+        public BreadcrumbItem(Item item)
+    : base(item) { Assert.IsNotNull(item, "item"); }
+
+        public string Title
+        { get { return InnerItem["Title"]; } }
+
+        public bool IsActive
+        { get { return Sitecore.Context.Item.ID == InnerItem.ID; } }
+
+        public string Url
+        { get { return LinkManager.GetItemUrl(InnerItem); } }
+    }
+}
---
+++ Cloudy8/Models/BreadcrumbItemList.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cloudy8.Models
+{
+    public class BreadcrumbItemList : Sitecore.Mvc.Presentation.RenderingModel
+    {
+        public List<BreadcrumbItem> Breadcrumbs { get; set; }
+        public override void Initialize(Rendering rendering)
+        {
+            Breadcrumbs = new List<BreadcrumbItem>();
+            List<Item> items = GetBreadcrumbItems();
+            foreach (Item item in items)
+            {
+                Breadcrumbs.Add(new BreadcrumbItem(item));
+            }
+            Breadcrumbs.Add(new BreadcrumbItem(Sitecore.Context.Item));
+        }
+
+        private List<Sitecore.Data.Items.Item> GetBreadcrumbItems()
+        {
+            string homePath = Sitecore.Context.Site.StartPath;
+            Item homeItem = Sitecore.Context.Database.GetItem(homePath);
+            List<Item> items = Sitecore.Context.Item.Axes.GetAncestors()
+              .SkipWhile(item => item.ID != homeItem.ID)
+              .ToList();
+            return items;
+        }
+    }
+}
